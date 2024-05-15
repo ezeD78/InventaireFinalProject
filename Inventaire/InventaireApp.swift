237c7ae8218b23e80271846a861b2793d90e587d
Ezequiel Gomes <ezequiel7897@gmail.com>
2024-05-15 11:05:09 +0200

    Initial Commit

---
+++ Inventaire/InventaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  InventaireApp.swift
+//  Inventaire
+//
+//  Created by Ezequiel Gomes on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct InventaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
